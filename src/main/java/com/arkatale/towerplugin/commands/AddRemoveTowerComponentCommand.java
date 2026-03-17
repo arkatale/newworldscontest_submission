92061960b05ff239af677bcf27b0f9af1f1ebc17
ArkaTale <arkatale1@gmail.com>
2026-03-17 17:04:07 +0100

    feat: how to add TowerComponent to player?

---
+++ src/main/java/com/arkatale/towerplugin/commands/AddRemoveTowerComponentCommand.java
@@ -0,0 +1,21 @@
+package com.arkatale.towerplugin.commands;
+
+import com.arkatale.towerplugin.component.TowerComponent;
+import com.hypixel.hytale.component.Component;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.jspecify.annotations.NonNull;
+
+public class AddRemoveTowerComponentCommand extends AbstractPlayerCommand {
+    @Override
+    protected void execute(@NonNull CommandContext commandContext, @NonNull Store<EntityStore> store, @NonNull Ref<EntityStore> ref, @NonNull PlayerRef playerRef, @NonNull World world) {
+        Component<EntityStore> towerComponent = new TowerComponent();
+//        store.addComponent(playerRef, TowerComponent.getComponentType());
+    }
+}
