32d944461f0e62fe862be18d9c56363e4513d225
ArkaTale <arkatale1@gmail.com>
2026-04-03 17:55:22 +0200

    wip(wtf am i doing?): trying to have multiple defendblocks have the same 2 second interval because otherwise it fills up to fast - and i have to remove the entities (todo) after adding to corruption

--- src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
+++ src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
@@ -2,13 +2,16 @@ package com.arkatale.defenseplugin.systems;
 
 import com.arkatale.defenseplugin.components.AttackerComponent;
 import com.arkatale.defenseplugin.components.DefendBlockComponent;
+import com.arkatale.defenseplugin.logic.DefendSession;
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.spatial.SpatialResource;
+import com.hypixel.hytale.component.system.tick.DelayedEntitySystem;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
+import com.hypixel.hytale.math.vector.Vector3i;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.modules.entity.EntityModule;
 import com.hypixel.hytale.server.core.universe.Universe;
@@ -18,12 +21,45 @@ import com.hypixel.hytale.server.npc.entities.NPCEntity;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DefendBlockSystem extends EntityTickingSystem<ChunkStore> {
+
+//    public static ArrayList<Float> keineAhnung;
+    public static Map<Integer, Float> keineAhnung = new ConcurrentHashMap<>();;
+
+//    public void DefendBlockSystem{
+////        keineAhnung = new ArrayList<Float>();
+//    }
+
     @Override
     public void tick(float v, int i, @NonNull ArchetypeChunk<ChunkStore> archetypeChunk, @NonNull Store<ChunkStore> store, @NonNull CommandBuffer<ChunkStore> commandBuffer) {
+
+        var current = keineAhnung.get(i);
+        //warum machst du das? weil es ist doch erstmal egal mit dem time zeug weil
+        // falls das jmd nutzen wird - wird es eh wahrscheinlich nur 1 concurrent block
+
+//        from DelayedEntitySystem
+//        DelayedEntitySystem.Data<ECS_TYPE> data = (DelayedEntitySystem.Data)store.getResource(this.resourceType);
+//        data.dt += dt;
+//        if (data.dt >= this.intervalSec) {
+//            float fullDt = data.dt;
+//            data.dt = 0.0F;
+//            super.tick(fullDt, systemIndex, store);
+//        }
+        var newValue = current + v;
+        keineAhnung.replace(i, newValue);
+
+        if(newValue >= 2f){
+            stuff(v, i, archetypeChunk, store, commandBuffer);
+        }
+    }
+        public void stuff(float v, int i, @NonNull ArchetypeChunk<ChunkStore> archetypeChunk, @NonNull Store<ChunkStore> store, @NonNull CommandBuffer<ChunkStore> commandBuffer) {
+
         var defendBlock = archetypeChunk.getComponent(i, DefendBlockComponent.getComponentType());
 
         if(defendBlock == null){
