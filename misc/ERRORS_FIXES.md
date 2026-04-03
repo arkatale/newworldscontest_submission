0b3568800091db33ea670fa42f3e25a5f1801122
ArkaTale <arkatale1@gmail.com>
2026-04-03 19:32:42 +0200

    fix(setBlock): by using BlockType.EMPTY_KEY instead of String.valueOf(BlockType.EMPTY)

--- misc/ERRORS_FIXES.md
+++ misc/ERRORS_FIXES.md
@@ -1,3 +1,14 @@
+world.setBlock(posI.x, posI.y, posI.z, String.valueOf(BlockType.EMPTY), 0);
+//[2026/04/03 17:29:36 SEVERE]         [World|default] Failed to run task!
+//java.lang.IllegalArgumentException: Unknown key! BlockType{id=Empty, ...}
+//	at com.hypixel.hytale.server.core.universe.world.accessor.BlockAccessor.setBlock(BlockAccessor.java:48)
+//	at com.hypixel.hytale.server.core.universe.world.accessor.IChunkAccessorSync.setBlock(IChunkAccessorSync.java:136)
+//	at com.arkatale.defenseplugin.systems.DefendBlockSystem.lambda$stuff$0(DefendBlockSystem.java:106)
+-> with
+world.setBlock(posI.x, posI.y, posI.z, BlockType.EMPTY_KEY, SetBlockSettings.NO_SEND_PARTICLES);
+
+
+
 [2026/04/02 17:23:00 SEVERE]                     [NPC|P] Error: java.lang.IllegalArgumentException: Asset 'Component_Instruction_Intelligent_Idle_Motion_Follow_Path'(368) is different type. Is 'com.hypixel.hytale.server.npc.instructions.Instruction' but should be 'com.hypixel.hytale.server.npc.instructions.ActionList' for NPC null
 -> Fix: 
     Instruction (with indent) instead of action without indent
--- src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
+++ src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
@@ -16,6 +16,7 @@ import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
 import com.hypixel.hytale.server.core.modules.entity.EntityModule;
 import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.SetBlockSettings;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
@@ -102,7 +103,17 @@ public class DefendBlockSystem extends EntityTickingSystem<ChunkStore> {
 
                     if(defendBlock.getCorruption() > 10){
 //                        Universe.get().sendMessage(Message.raw("You loose"));
-                        world.setBlock(posI.x, posI.y, posI.z, String.valueOf(BlockType.EMPTY));
+//
+//                        world.execute(() -> {
+                            world.setBlock(posI.x, posI.y, posI.z, BlockType.EMPTY_KEY, SetBlockSettings.NO_SEND_PARTICLES);
+                            //^^ fixed by BlockType.Empty_Key instead of String.valueOf(BlockType.EMPTY)
+                            //[2026/04/03 17:29:36 SEVERE]         [World|default] Failed to run task!
+                            //java.lang.IllegalArgumentException: Unknown key! BlockType{id=Empty, unknown=false, group='Air', blockSoundSetId='EMPTY', blockSoundSetIndex=0, particles=null, blockParticleSetId='null', blockBreakingDecalId='null', particleColor=null, effect=null, textures=null, textureSideMask='null', cubeShadingMode=Standard, customModel='null', customModelTexture=null, customModelScale=1.0, customModelAnimation='null', drawType=Empty, material=Empty, opacity=Transparent, requiresAlphaBlending=false, tickProcedurenull, tintUp=null, tintDown=null, tintNorth=null, tintSouth=null, tintWest=null, tintEast=null, biomeTintUp=0, biomeTintDown=0, biomeTintNorth=0, biomeTintSouth=0, biomeTintWest=0, biomeTintEast=0, randomRotation=None, variantRotation=None, flipType=SYMMETRIC, rotationYawPlacementOffset=None, transitionTexture='null', transitionToGroups=null, hitboxType='Full', hitboxTypeIndex=0, interactionHitboxType='null', interactionHitboxTypeIndex=-2147483648, light=null, movementSettings=BlockMovementSettings{isClimbable=falseisBouncy=falsebounceSpeed=0.0, climbUpSpeedMultiplier=1.0, climbDownSpeedMultiplier=1.0, climbLateralSpeedMultiplier=1.0, drag=0.82, friction=0.18, terminalVelocityModifier=1.0, horizontalSpeedMultiplier=1.0, jumpForceMultiplier=1.0}, flags=com.hypixel.hytale.protocol.BlockFlags@9e5b, interactionHint='null', isTrigger=false, damageToEntities=0, allowsMultipleUsers=true, bench=null, gathering=null, placementSettings=null, state=null, ambientSoundEventId='null', ambientSoundEventIndex='0', conditionalSounds=null, interactionSoundEventId='null', interactionSoundEventIndex='0', isLooping=false, farming=null, supportDropType=BREAK, maxSupportDistance=0, support={}, supporting={}, interactions={}, railConfig=null}
+                            //	at com.hypixel.hytale.server.core.universe.world.accessor.BlockAccessor.setBlock(BlockAccessor.java:48)
+                            //	at com.hypixel.hytale.server.core.universe.world.accessor.IChunkAccessorSync.setBlock(IChunkAccessorSync.java:136)
+                            //	at com.arkatale.defenseplugin.systems.DefendBlockSystem.lambda$stuff$0(DefendBlockSystem.java:106)
+
+//                        });
                         return;
                     }
 
