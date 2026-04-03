37269730c463014ddf84ecacd1c3a432ec1252d4
ArkaTale <arkatale1@gmail.com>
2026-04-03 12:28:04 +0200

    feat: DefendBlockSystem.java with Query DefendBlockComponent - can't i just query for the DefendBlockComponent? or do i have to do this archetype stuff?

--- src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
+++ src/main/java/com/arkatale/defenseplugin/systems/DefendBlockSystem.java
@@ -1,24 +1,24 @@
-//package com.arkatale.defenseplugin.systems;
-//
-//import com.arkatale.defenseplugin.components.AttackerComponent;
-//import com.hypixel.hytale.component.ArchetypeChunk;
-//import com.hypixel.hytale.component.CommandBuffer;
-//import com.hypixel.hytale.component.Store;
-//import com.hypixel.hytale.component.query.Query;
-//import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
-//import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
-//import org.jspecify.annotations.NonNull;
-//import org.jspecify.annotations.Nullable;
-//
-//public class DefendBlockSystem extends EntityTickingSystem<ChunkStore> {
-//    @Override
-//    public void tick(float v, int i, @NonNull ArchetypeChunk<ChunkStore> archetypeChunk, @NonNull Store<ChunkStore> store, @NonNull CommandBuffer<ChunkStore> commandBuffer) {
-//;
-//    }
-//
-//    @Override
-//    public @Nullable Query<ChunkStore> getQuery() {
-////        return Query.and(Defe.getComponentType());
-////        ^^fixed java.lang.IllegalArgumentException: Query in AndQuery cannot be null (Index: 0)
-//    }
-//}
+package com.arkatale.defenseplugin.systems;
+
+import com.arkatale.defenseplugin.components.AttackerComponent;
+import com.arkatale.defenseplugin.components.DefendBlockComponent;
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
+import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+public class DefendBlockSystem extends EntityTickingSystem<ChunkStore> {
+    @Override
+    public void tick(float v, int i, @NonNull ArchetypeChunk<ChunkStore> archetypeChunk, @NonNull Store<ChunkStore> store, @NonNull CommandBuffer<ChunkStore> commandBuffer) {
+        var test = "";
+    }
+
+    @Override
+    public @Nullable Query<ChunkStore> getQuery() {
+        return Query.and(DefendBlockComponent.getComponentType());
+    }
+}
